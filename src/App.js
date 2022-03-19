5796432f072af592eb2e56f804bef62c6e6ac6d5
sametkoyuncu <sametkoyuncu@live.com>
2022-03-19 20:31:26 +0300

    Add soft ui dashboard

--- src/App.js
+++ src/App.js
@@ -1,49 +1,142 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Sidebar from './components/Sidebar'
+/**
+=========================================================
+* Soft UI Dashboard React - v3.1.0
+=========================================================
 
-const listItems = [
-  { title: 'Dashboard', src: 'Chart_fill' },
-  { title: 'Inbox', src: 'Chat' },
-  { title: 'Accounts', src: 'User', gap: true },
-  { title: 'Schedule ', src: 'Calendar' },
-  { title: 'Search', src: 'Search' },
-  { title: 'Analytics', src: 'Chart' },
-  { title: 'Files ', src: 'Folder', gap: true },
-  { title: 'Setting', src: 'Setting' },
-]
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
 
-function App() {
-  const [width, setWidth] = useState(window.innerWidth)
+Coded by www.creative-tim.com
 
-  const [isOpen, setIsOpen] = useState(width > 640 ? true : false)
+ =========================================================
 
-  const updateDimensions = () => {
-    setWidth(window.innerWidth)
-  }
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
 
+import { useState, useEffect } from "react";
+
+// react-router components
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+// @mui material components
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Icon from "@mui/material/Icon";
+
+// Soft UI Dashboard React components
+import SuiBox from "components/SuiBox";
+
+// Soft UI Dashboard React examples
+import Sidenav from "examples/Sidenav";
+import Configurator from "examples/Configurator";
+
+// Soft UI Dashboard React themes
+import theme from "assets/theme";
+
+// Soft UI Dashboard React routes
+import routes from "routes";
+
+// Soft UI Dashboard React contexts
+import { useSoftUIController, setMiniSidenav, setOpenConfigurator } from "context";
+
+// Images
+import brand from "assets/images/c-logo.png";
+
+export default function App() {
+  const [controller, dispatch] = useSoftUIController();
+  const { miniSidenav, direction, layout, openConfigurator, sidenavColor } = controller;
+  const [onMouseEnter, setOnMouseEnter] = useState(false);
+  const { pathname } = useLocation();
+
+  // Open sidenav when mouse enter on mini sidenav
+  const handleOnMouseEnter = () => {
+    if (miniSidenav && !onMouseEnter) {
+      setMiniSidenav(dispatch, false);
+      setOnMouseEnter(true);
+    }
+  };
+
+  // Close sidenav when mouse leave mini sidenav
+  const handleOnMouseLeave = () => {
+    if (onMouseEnter) {
+      setMiniSidenav(dispatch, true);
+      setOnMouseEnter(false);
+    }
+  };
+
+  // Change the openConfigurator state
+  const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
+
+  // Setting the dir attribute for the body element
   useEffect(() => {
-    window.addEventListener('resize', updateDimensions)
-    return () => window.removeEventListener('resize', updateDimensions)
-  }, [])
+    document.body.setAttribute("dir", direction);
+  }, [direction]);
 
+  // Setting page scroll to 0 when changing the route
   useEffect(() => {
-    if (width < 640) setIsOpen(false)
-    else setIsOpen(true)
-  }, [width])
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+  }, [pathname]);
+
+  const getRoutes = (allRoutes) =>
+    allRoutes.map((route) => {
+      if (route.collapse) {
+        return getRoutes(route.collapse);
+      }
+
+      if (route.route) {
+        return <Route exact path={route.route} element={route.component} key={route.key} />;
+      }
+
+      return null;
+    });
+
+  const configsButton = (
+    <SuiBox
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="3.5rem"
+      height="3.5rem"
+      bgColor="white"
+      shadow="sm"
+      borderRadius="50%"
+      position="fixed"
+      right="2rem"
+      bottom="2rem"
+      zIndex={99}
+      color="dark"
+      sx={{ cursor: "pointer" }}
+      onClick={handleConfiguratorOpen}
+    >
+      <Icon fontSize="default" color="inherit">
+        settings
+      </Icon>
+    </SuiBox>
+  );
 
   return (
-    <div className="flex">
-      <Sidebar
-        isOpen={isOpen}
-        setIsOpen={setIsOpen}
-        listItems={listItems}
-      ></Sidebar>
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">
-        <h1>Home Page</h1>
-      </div>
-    </div>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {layout === "dashboard" && (
+        <>
+          <Sidenav
+            color={sidenavColor}
+            brand={brand}
+            brandName="ColorTown"
+            routes={routes}
+            onMouseEnter={handleOnMouseEnter}
+            onMouseLeave={handleOnMouseLeave}
+          />
+          <Configurator />
+          {configsButton}
+        </>
+      )}
+      {/* {layout === "vr" && <Configurator />} */}
+      <Routes>
+        {getRoutes(routes)}
+        <Route path="*" element={<Navigate to="/colors" />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
-
-export default App
