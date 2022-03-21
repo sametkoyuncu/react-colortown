9dbb3d734e3ee28dee612901104bd1fae54bb9ea
sametkoyuncu <sametkoyuncu@live.com>
2022-03-21 21:22:26 +0300

    remove unusing states

--- src/context/colortown/index.js
+++ src/context/colortown/index.js
@@ -7,37 +7,35 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ColorTownContext = createContext();
 
 export const ColorTownProvider = ({ children }) => {
-  const [ctColors, setCtColors] = useState(
-    // eslint-disable-next-line no-array-constructor
-    JSON.parse(localStorage.getItem("ctColors")) || new Array()
-  );
-  const [ctGradients, setCtGradients] = useState(
-    JSON.parse(localStorage.getItem("ctGradients") || [null])
-  );
-  const [ctPalettes, setCtPalettes] = useState(
-    JSON.parse(localStorage.getItem("ctPalettes") || [null])
-  );
+  const [ctColors, setCtColors] = useState(JSON.parse(localStorage.getItem("ctColors")) || []);
+  // const [ctGradients, setCtGradients] = useState(
+  //   JSON.parse(localStorage.getItem("ctGradients") || [])
+  // );
+  // const [ctPalettes, setCtPalettes] = useState(
+  //   JSON.parse(localStorage.getItem("ctPalettes") || [])
+  // );
 
   useEffect(() => {
     localStorage.setItem("ctColors", JSON.stringify(ctColors));
   }, [ctColors]);
 
-  useEffect(() => {
-    localStorage.setItem("ctGradients", JSON.stringify(ctGradients));
-  }, [ctGradients]);
+  // useEffect(() => {
+  //   localStorage.setItem("ctGradients", JSON.stringify(ctGradients));
+  // }, [ctGradients]);
 
-  useEffect(() => {
-    localStorage.setItem("ctPalettes", JSON.stringify(ctPalettes));
-  }, [ctPalettes]);
+  // useEffect(() => {
+  //   localStorage.setItem("ctPalettes", JSON.stringify(ctPalettes));
+  // }, [ctPalettes]);
 
   const values = {
     ctColors,
-    ctGradients,
-    ctPalettes,
     setCtColors,
-    setCtGradients,
-    setCtPalettes,
   };
+
+  // ctGradients,
+  // ctPalettes,
+  // setCtGradients,
+  // setCtPalettes,
   return <ColorTownContext.Provider value={values}>{children}</ColorTownContext.Provider>;
 };
 
