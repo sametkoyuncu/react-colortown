df94e6dac34352cd7d2640549630d3e2585ba617
sametkoyuncu <sametkoyuncu@live.com>
2022-03-21 15:17:35 +0300

    Add colors data

---
+++ src/data/colors.js
@@ -0,0 +1,46 @@
+const colors = [
+  {
+    id: "color_1",
+    hexCode: "#f857a6",
+    likes: 12,
+    tags: [],
+  },
+  {
+    id: "color_2",
+    hexCode: "#ff5858",
+    likes: 30,
+    tags: [],
+  },
+  {
+    id: "color_3",
+    hexCode: "#4b6cb7",
+    likes: 45,
+    tags: [],
+  },
+  {
+    id: "color_4",
+    hexCode: "#182848",
+    likes: 34,
+    tags: [],
+  },
+  {
+    id: "color_5",
+    hexCode: "#FC354C",
+    likes: 10,
+    tags: [],
+  },
+  {
+    id: "color_6",
+    hexCode: "#0ABFBC",
+    likes: 7,
+    tags: [],
+  },
+  {
+    id: "color_7",
+    hexCode: "#03C03C",
+    likes: 87,
+    tags: [],
+  },
+];
+
+export default colors;
