5829bef5623acbfb6c2f18dcd9fa038283959517
sametkoyuncu <sametkoyuncu@live.com>
2022-03-28 22:46:43 +0300

    Add tags section to color details page

---
+++ src/components/CtColorTagsCard/index.js
@@ -0,0 +1,40 @@
+// @mui material components
+import Card from "@mui/material/Card";
+
+// Soft UI Dashboard React components
+import SuiBox from "components/SuiBox";
+
+// prop-types is a library for typechecking of props
+import PropTypes from "prop-types";
+
+//
+import Tag from "./tag";
+
+function ColorTagsCard({ tags }) {
+  return (
+    <Card sx={{ height: "100%", padding: "0.75rem", paddingBottom: "0" }}>
+      <SuiBox display="flex" alignItems="center" justifyContent="center" flexWrap="wrap">
+        {tags.map((tag) => (
+          <Tag bgColor={tag.color} label={tag.label} key={tag.color} />
+        ))}
+      </SuiBox>
+    </Card>
+  );
+}
+
+ColorTagsCard.defaultProps = {
+  tags: [
+    {
+      color: "",
+      label: "",
+    },
+  ],
+};
+
+// Typechecking props for the SuiBox
+ColorTagsCard.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  tags: PropTypes.array,
+};
+
+export default ColorTagsCard;
---
+++ src/data/tags.js
@@ -0,0 +1,16 @@
+// Blue Green Red Pink Black Yellow Grey Orange White Brown Purple Beige
+const tags = [
+  { color: "#4D77FF", label: "Blue" },
+  { color: "#8BDB81", label: "Green" },
+  { color: "#FD5D5D", label: "Red" },
+  { color: "#FF7BA9", label: "Pink" },
+  { color: "#3A3845", label: "Black" },
+  { color: "#FFE162", label: "Yellow" },
+  { color: "#DFDFDE", label: "Grey" },
+  { color: "#FF7F3F", label: "Orange" },
+  { color: "#FEFEFE", label: "White" },
+  { color: "#AD8B73", label: "Brown" },
+  { color: "#A63EC5", label: "Purple" },
+  { color: "#F6E7D8", label: "Beige" },
+];
+export default tags;
