8a74fc10ca7c9f5f8dd939ef31e9f1cf72998ec5
sametkoyuncu <sametkoyuncu@live.com>
2022-04-07 23:46:24 +0300

    Add sign up with email and password

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -16,7 +16,10 @@ Coded by www.creative-tim.com
 import { useState } from "react";
 
 // react-router-dom components
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+// firebase
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 // @mui material components
 import Card from "@mui/material/Card";
@@ -36,10 +39,40 @@ import BasicLayout from "layouts/authentication/components/BasicLayout";
 // Images
 import curved6 from "assets/images/curved-images/curved14.jpg";
 
+// firebase.js
+import { auth } from "../../../firebase";
+
 function SignUp() {
   const [agreement, setAgremment] = useState(true);
 
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
   const handleSetAgremment = () => setAgremment(!agreement);
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    setError(false);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // sign up
+        // eslint-disable-next-line prefer-destructuring, no-unused-vars
+        const user = userCredential.user;
+        navigate("/authentication/sign-in");
+      })
+      .catch((err) => {
+        // eslint-disable-next-line no-unused-vars
+        const errorCode = err.code;
+        // eslint-disable-next-line no-unused-vars
+        const errorMsg = err.message;
+        setErrorMessage(err.message);
+        setError(true);
+      });
+  };
 
   const inputStyles = { border: "1px solid #e5e5e5", borderRadius: "8px" };
 
@@ -47,16 +80,39 @@ function SignUp() {
     <BasicLayout title="Welcome!" image={curved6}>
       <Card>
         <SuiBox pt={2} pb={3} px={3}>
-          <SuiBox component="form" role="form">
-            <SuiBox mb={2}>
+          <SuiBox component="form" role="form" onSubmit={handleSignUp}>
+            {/* <SuiBox mb={2}>
               <SuiInput placeholder="Username" sx={inputStyles} />
+            </SuiBox> */}
+            <SuiBox mb={2}>
+              <SuiInput
+                type="email"
+                minLength="6"
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                sx={inputStyles}
+              />
             </SuiBox>
             <SuiBox mb={2}>
-              <SuiInput type="email" placeholder="Email" sx={inputStyles} />
+              <SuiInput
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                sx={inputStyles}
+              />
             </SuiBox>
-            <SuiBox mb={2}>
-              <SuiInput type="password" placeholder="Password" sx={inputStyles} />
-            </SuiBox>
+            {error && (
+              <SuiBox mb={3}>
+                <SuiTypography variant="button" color="error" fontWeight="regular">
+                  Upps! Something look likes wrong. Please, try again.{" "}
+                </SuiTypography>
+                <br />
+                <SuiTypography variant="button" color="error" fontWeight="regular">
+                  {/* cut 'Firebase: ' part from error message */}
+                  {/:(.+)/.exec(errorMessage)[1]}
+                </SuiTypography>
+              </SuiBox>
+            )}
             <SuiBox display="flex" alignItems="center">
               <Checkbox checked={agreement} onChange={handleSetAgremment} disabled />
               <SuiTypography
@@ -72,7 +128,7 @@ function SignUp() {
               </SuiTypography>
             </SuiBox>
             <SuiBox mt={4} mb={1}>
-              <SuiButton variant="gradient" color="dark" fullWidth>
+              <SuiButton type="submit" variant="gradient" color="dark" fullWidth>
                 sign up
               </SuiButton>
             </SuiBox>
