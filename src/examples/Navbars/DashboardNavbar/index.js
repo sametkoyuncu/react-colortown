4161b9f3ceaed63bde233c62956efff6f5673ba7
sametkoyuncu <sametkoyuncu@live.com>
2022-04-16 04:45:01 +0300

    Remove unnecessary files

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -25,18 +25,15 @@ import PropTypes from "prop-types";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-// import Menu from "@mui/material/Menu";
 import Icon from "@mui/material/Icon";
 
 // Soft UI Dashboard React components
 import SuiBox from "components/SuiBox";
 import CtAccountNavbarMenu from "components/CtAccountNavbarMenu";
-// import SuiTypography from "components/SuiTypography";
 import SuiInput from "components/SuiInput";
 
 // Soft UI Dashboard React examples
 import Breadcrumbs from "examples/Breadcrumbs";
-// import NotificationItem from "examples/Items/NotificationItem";
 
 // Custom styles for DashboardNavbar
 import {
@@ -51,18 +48,10 @@ import {
 // Soft UI Dashboard React context
 import { useSoftUIController, setTransparentNavbar, setMiniSidenav } from "context";
 
-// setOpenConfigurator,
-
-// Images
-// import team2 from "assets/images/team-2.jpg";
-// import logoSpotify from "assets/images/small-logos/logo-spotify.svg";
-
 function DashboardNavbar({ absolute, light, isMini }) {
   const [navbarType, setNavbarType] = useState();
   const [controller, dispatch] = useSoftUIController();
   const { miniSidenav, transparentNavbar, fixedNavbar } = controller;
-  // const { miniSidenav, transparentNavbar, fixedNavbar, openConfigurator } = controller;
-  // const [openMenu, setOpenMenu] = useState(false);
   const route = useLocation().pathname.split("/").slice(1);
 
   useEffect(() => {
@@ -92,48 +81,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
   }, [dispatch, fixedNavbar]);
 
   const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
-  // const handleConfiguratorOpen = () => setOpenConfigurator(dispatch, !openConfigurator);
-  // const handleOpenMenu = (event) => setOpenMenu(event.currentTarget);
-  // const handleCloseMenu = () => setOpenMenu(false);
-
-  // Render the notifications menu
-  // const renderMenu = () => (
-  //   <Menu
-  //     anchorEl={openMenu}
-  //     anchorReference={null}
-  //     anchorOrigin={{
-  //       vertical: "bottom",
-  //       horizontal: "left",
-  //     }}
-  //     open={Boolean(openMenu)}
-  //     onClose={handleCloseMenu}
-  //     sx={{ mt: 2 }}
-  //   >
-  //     <NotificationItem
-  //       image={<img src={team2} alt="person" />}
-  //       title={["New message", "from Laur"]}
-  //       date="13 minutes ago"
-  //       onClick={handleCloseMenu}
-  //     />
-  //     <NotificationItem
-  //       image={<img src={logoSpotify} alt="person" />}
-  //       title={["New album", "by Travis Scott"]}
-  //       date="1 day"
-  //       onClick={handleCloseMenu}
-  //     />
-  //     <NotificationItem
-  //       color="secondary"
-  //       image={
-  //         <Icon fontSize="small" sx={{ color: ({ palette: { white } }) => white.main }}>
-  //           payment
-  //         </Icon>
-  //       }
-  //       title={["", "Payment successfully completed"]}
-  //       date="2 days"
-  //       onClick={handleCloseMenu}
-  //     />
-  //   </Menu>
-  // );
 
   return (
     <AppBar
@@ -156,24 +103,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
             </SuiBox>
             <SuiBox sx={{ display: "flex", justifyContent: "flex-end" }}>
               <SuiBox color={light ? "white" : "inherit"}>
-                {/* <Link to="/authentication/sign-in">
-                <IconButton sx={navbarIconButton} size="small">
-                  <Icon
-                    sx={({ palette: { dark, white } }) => ({
-                      color: light ? white.main : dark.main,
-                    })}
-                  >
-                    account_circle
-                  </Icon>
-                  <SuiTypography
-                    variant="button"
-                    fontWeight="medium"
-                    color={light ? "white" : "dark"}
-                  >
-                    Sign in
-                  </SuiTypography>
-                </IconButton>
-              </Link> */}
                 <IconButton
                   size="small"
                   color="inherit"
@@ -184,26 +113,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
                     {miniSidenav ? "menu_open" : "menu"}
                   </Icon>
                 </IconButton>
-                {/* <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                onClick={handleConfiguratorOpen}
-              >
-                <Icon>settings</Icon>
-              </IconButton> */}
-                {/* <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon className={light ? "text-white" : "text-dark"}>notifications</Icon>
-              </IconButton> */}
-                {/* {renderMenu()} */}
               </SuiBox>
               <CtAccountNavbarMenu />
             </SuiBox>
--- src/routes.js
+++ src/routes.js
@@ -36,33 +36,17 @@ Coded by www.creative-tim.com
 */
 
 // Soft UI Dashboard React layouts
-// import Dashboard from "layouts/dashboard";
 import Colors from "layouts/colors";
 import Gradients from "layouts/gradients";
 import Palettes from "layouts/palettes";
 import ColorGenerate from "layouts/colorGenerate";
 import GradientGenerate from "layouts/gradientGenerate";
 import PaletteGenerate from "layouts/paletteGenerate";
-// import Tables from "layouts/tables";
-// import Billing from "layouts/billing";
-// import VirtualReality from "layouts/virtual-reality";
-// import RTL from "layouts/rtl";
-// import Profile from "layouts/profile";
-// import SignIn from "layouts/authentication/sign-in";
-// import SignUp from "layouts/authentication/sign-up";
 
 // Soft UI Dashboard React icons
-// import Shop from "examples/Icons/Shop";
-// import Office from "examples/Icons/Office";
-// import Settings from "examples/Icons/Settings";
 import ColorLensIcon from "@mui/icons-material/ColorLens";
 import GradientIcon from "@mui/icons-material/Gradient";
 import StyleIcon from "@mui/icons-material/Style";
-// import Document from "examples/Icons/Document";
-// import SpaceShip from "examples/Icons/SpaceShip";
-// import CustomerSupport from "examples/Icons/CustomerSupport";
-// import CreditCard from "examples/Icons/CreditCard";
-// import Cube from "examples/Icons/Cube";
 
 const routes = [
   { type: "title", title: "Collections", key: "collections" },
